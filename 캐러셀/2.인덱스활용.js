b8f6f901fa4a511a23cedd5cebafe9a29d954b2b
ccjihyeon <130423140+ccjihyeon@users.noreply.github.com>
2023-10-27 12:57:51 +0900

    10.27

--- 캐러셀/2.인덱스활용.js
+++ 캐러셀/2.인덱스활용.js
@@ -1,14 +1,15 @@
 const contentsWrapper = document.querySelector('.contents-wrapper');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
+const 슬라이드갯수 = contentsWrapper.childElementCount
+
 const nowPage = document.querySelector('.now-page');
 const totelPage = document.querySelector('.totel-page');
 
 let index = 0; 
-// childElementCount에는 html요소가 가진 자식의 갯수가 들어있다.
-const 슬라이드갯수 = contentsWrapper.childElementCount
 
-// 이전버튼
+
+totelPage.innerText = 슬라이더갯수;
 prevButton.addEventListener('click',()=>{
   
     index--;  
@@ -17,7 +18,7 @@ prevButton.addEventListener('click',()=>{
     }
     위치적용();
 })
-// 다음버튼
+
 nextButton.addEventListener('click',()=>{
     
     index++;
@@ -31,10 +32,7 @@ function 위치적용() {
 
     nowPage.innerText = index+1;
 }
-//  index 0 : 값 0
-//  inadex 1 : 값 -300
-//  index 2 : 값 -600
-//  위 규칙을 식으로 나타내면 index * -300 이다 
-totelPage.innerText = 슬라이더갯수;
+
+
 
 
